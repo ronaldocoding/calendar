2085dede9f2928e75ad4faefb259a5a66907f19f
ronaldocosta <ronaldocosta.developer@gmail.com>
2023-01-09 14:58:04 -0400

    Implement register

--- app.py
+++ app.py
@@ -3,11 +3,14 @@ import sqlite3
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+USER_NOT_LOGGED_STATUS = 1
 
 
-@app.route('/login.html', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
+    if request.method == 'GET':
+        return render_template('login.html')
+    elif request.method == 'POST':
         connection = sqlite3.connect('/Users/ronaldocosta/Documents/pessoal/calendar/agenda.db')
         cursor = connection.cursor()
 
@@ -24,11 +27,28 @@ def login():
         else:
             cursor.execute("UPDATE agenda_usuario SET usuario_status = 2 WHERE usuario_email = '"+user_email+"'")
             connection.commit()
-            return render_template('register.html')
+            connection.close()
+            return render_template('home.html')
 
-    return render_template('login.html')
 
-
-@app.route('/register.html')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html')
+    if request.method == 'GET':
+        return render_template('register.html')
+    elif request.method == 'POST':
+        connection = sqlite3.connect('/Users/ronaldocosta/Documents/pessoal/calendar/agenda.db')
+        cursor = connection.cursor()
+
+        query = "INSERT INTO agenda_usuario (usuario_nome, usuario_email, usuario_senha, usuario_status) VALUES( ?, ?, ?, ?)"
+        data_tuple = (request.form['name'], request.form['email'], request.form['password'], USER_NOT_LOGGED_STATUS)
+
+        cursor.execute(query, data_tuple)
+        connection.commit()
+        connection.close()
+
+        return render_template('login.html')
+
+
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    return render_template('home.html')
